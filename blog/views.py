c9666068b7f7bc910dc40508b78c7279773ab16e
ebin0402 <ebin0402@gmail.com>
2020-06-09 22:38:35 +0530

    added Data

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render
-
-posts =[
-    {
-        'title':'Data Science',
-        'author':'Ebin Baby',
-        'Date':'June 2020',
-        'Content':'Data science is cool!!',
-    },
-    {
-        'title':'Machine Learning',
-        'author':'Ebin Baby',
-        'Date':'June 2020',
-        'Content':'Machine Learning is Amazing!!',
-    },
-    {
-        'title':'Python',
-        'author':'Ebin Baby',
-        'Date':'June 2020',
-        'Content':'Python is incredible!!',
-    }
-]
+from .models import Post
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 def about(request):
